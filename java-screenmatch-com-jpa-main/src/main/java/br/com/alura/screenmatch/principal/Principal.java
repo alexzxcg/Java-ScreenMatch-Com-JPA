8a977096a359d4a99c9d08a59a0e7ad4f816a016
Alex <alex-xp123@hotmail.com>
2024-10-01 15:02:11 -0300

    Implementado metodo que lista o top 5 series com base na avaliacao

--- java-screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ java-screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -40,6 +40,7 @@ public class Principal {
                     3 - Listar séries buscadas
                     4 - Buscar serie por titulo
                     5 - Buscar séries por ator
+                    6 - Top 5 séries
                     
                     0 - Sair                                 
                     """;
@@ -63,6 +64,9 @@ public class Principal {
                     break;
                 case 5:
                     buscarSeriePorAtor();
+                    break;
+                case 6:
+                    buscarTop5series();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -149,5 +153,10 @@ public class Principal {
         List<Serie> seriesEscontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
         System.out.println("Series em que " + nomeAtor + " trabalhou: ");
         seriesEscontradas.forEach(s -> System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarTop5series() {
+        List<Serie> seriesTop = repositorio.findTop5ByOrderByAvaliacaoDesc();
+        seriesTop.forEach(s -> System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
     }
 }
